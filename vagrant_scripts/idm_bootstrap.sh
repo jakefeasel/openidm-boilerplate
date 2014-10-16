b06716ef9e8fe54f0325d0ae6ad40de374b7d71b
jakefeasel <jfeasel@gmail.com>
2014-10-15 19:53:08 -0700

    Removing unnecessary hostfile entries

--- vagrant_scripts/idm_bootstrap.sh
+++ vagrant_scripts/idm_bootstrap.sh
@@ -3,9 +3,6 @@
 export OPENIDM_OPTS="-Xms128m -Xmx256m"
 
 echo "192.168.50.4 OPENIDM_REPO_HOST" >> /etc/hosts
-echo "192.168.50.4 SQLFIDDLE_HOST" >> /etc/hosts
-echo "192.168.50.4 POSTGRESQL93_HOST" >> /etc/hosts
-echo "192.168.50.5 MYSQL56_HOST" >> /etc/hosts
 
 echo "export OPENIDM_OPTS=\"${OPENIDM_OPTS}\"" >> /etc/profile
 
